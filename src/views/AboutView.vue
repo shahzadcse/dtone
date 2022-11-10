7d2586052b4236f5bc5f4f5447b8a2e6169830b3
Shahzad Hussain <shahzadcse@gmail.com>
2022-11-10 11:41:15 +0530

    add about content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About this POC</h1>
+    <p>
+      Here are some requirements to guide you through: (there are no right or
+      wrong answers, just your intuition and creativity in integrating and
+      offering the API):
+    </p>
+
+    <ul>
+      <li>
+        A. A user section that discovers and buys products, as well as running
+        promotions.
+      </li>
+      <li>
+        B. An administrator section that displays Megasoft’s available Balances
+        and Transaction reports.
+      </li>
+      <li>
+        C. You can use your preferred technology to do this implementation (DT
+        One adopted Vue.Js for its front-end projects).
+      </li>
+      <li>
+        D. Let’s assume the Product Manage falls in love with your POC, what
+        steps would you take to move it to production?
+      </li>
+    </ul>
+    <br />
+
+    <p>Applicant Details</p>
+    <h2>Shahzad Hussain</h2>
+    <h4>Sr. Front End Developer</h4>
+    <a href="mailto:shahzadcse@gmail.com">shahzadcse@gmail.com</a>
+    <p>Contact : <a href="tel:+919829183783"> +91 98291 83783</a></p>
   </div>
 </template>
 
